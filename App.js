04e17146e264570b46b09ba14a83434201eaa9da
sadegh <akbarisadegh382@gmail.com>
2021-11-29 00:35:59 +0330

    'start'

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import React , {useEffect} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import SplashScreen from 'react-native-splash-screen'
+import React from 'react';
+import { Login, Recipe } from "./screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
 
+import Tabs from "./navigation/tabs";
+
+const Stack = createStackNavigator();
 
 const App = () => {
-
-  useEffect(() => {
-    SplashScreen.hide()
-  }, [])
-  return (
-    <View>
-      <Text>Food App</Text>
-    </View>
-  )
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'Login'}
+            >
+                <Stack.Screen
+                    name="Login"
+                    component={Login}
+                />
+                <Stack.Screen
+                    name="Home"
+                    component={Tabs}
+                />
+                <Stack.Screen
+                    name="Recipe"
+                    component={Recipe}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
