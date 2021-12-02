5c413e098dcff48a52b804e35f37a1542660693f
sadegh <akbarisadegh382@gmail.com>
2021-12-03 01:49:13 +0330

    'recipe'

--- screens/Recipe.js
+++ screens/Recipe.js
@@ -1,12 +1,19 @@
-import React ,{useState , useEffect} from 'react';
+import React ,{useState , useEffect , useRef} from 'react';
 import {
     View,
-    Text
+    Text,
+    TouchableOpacity,
+    FlatList,
+    Animated,
+    Image
 } from 'react-native';
+import { COLORS , FONTS , icons, SIZES } from '../constants';
+
+const HEADER_HEIGHT = 350;
 
 const Recipe = ({route , navigation}) => {
     const [selectedRecipe, setSelectedRecipe] = useState(null)
-
+    const scrollY = useRef(new Animated.Value(0)).current;
     useEffect(()=>{
         setSelectedRecipe(route.params.recipe)
     },[])
@@ -18,11 +25,79 @@ const Recipe = ({route , navigation}) => {
         <View
             style={{
                 flex: 1,
-                alignItems: 'center',
-                justifyContent: 'center'
+                backgroundColor:COLORS.white
             }}
         >
-            <Text>Recipe {selectedRecipe.name}</Text>
+            <Animated.FlatList
+                data={selectedRecipe?.ingredients}
+                keyExtractor={item =>`${item.id}`}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View>
+                        {/* header */}
+                        {/* info */}
+                        {/* title */}
+                    </View>
+                }
+                scrollEventThrottle={16}
+                onScroll={Animated.event([
+                    {nativeEvent : {contentOffset : {y:scrollY}}}
+                ] , {useNativeDriver : true})}
+                renderItem={({item , index})=>{
+                    return(
+                        <View 
+                            style={{
+                                flexDirection:'row',
+                                paddingHorizontal:SIZES.base,
+                                alignItems:'center',
+                                marginVertical:SIZES.base,
+                                paddingHorizontal:SIZES.padding
+                            }}
+                        >
+                            <View
+                                style={{
+                                    backgroundColor:COLORS.lightGray,
+                                    borderRadius:50,
+                                    padding : SIZES.base,
+                                    justifyContent:'center',
+                                    alignItems:'center'
+                                }}
+                            >
+                                <Image
+                                    source={item.icon}
+                                    style={{
+                                        width :30,
+                                        height : 30
+                                    }}
+                                />
+                            </View>
+                            <View>
+                                <Text
+                                    style={{
+                                        color:COLORS.black,
+                                        ...FONTS.body3,
+                                        marginLeft : 10
+                                    }}
+                                >{item.description}</Text>
+                            </View>
+                            <View
+                                style={{
+                                    flex:1,
+                                    alignItems:'flex-end',
+                                    justifyContent:'center'
+                                }}
+                            >
+                                <Text 
+                                    style={{
+                                        color:COLORS.black,
+                                        ...FONTS.body5
+                                    }}
+                                >{item.quantity}</Text>
+                            </View>
+                        </View>
+                    )
+                }}
+            />
         </View>
     )
 }
